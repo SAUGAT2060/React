7769f40462a890575297f6bd4083af7466744300
Saugat Rana <ranasaugat9@gmail.com>
2025-05-25 16:11:49 -0400

    Learned about creating custom react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from './chai.jsx';
 function App() {
 
+  const apple='Apple and Apples';
  
 
   return (
     <>
      <Chai/>
-     <h1>Yo</h1>
+     <h1>Yo {apple }</h1>
      <p>K xa yar</p>
     
     </>
