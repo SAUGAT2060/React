cc0263fb4134ac8c9ef0a32982b2fddfc6963a96
Saugat Rana <ranasaugat9@gmail.com>
2025-06-05 09:33:05 -0400

    Finished the miniContextAPI project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -4,8 +4,6 @@ import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
 import Profile from './components/Profile'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <UserContextProvider>
       <h1>React for ContextAPI</h1>
