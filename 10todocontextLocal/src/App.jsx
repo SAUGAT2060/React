86a7385cb74de1c1d7c9c6c0add897fd7ce6f898
Saugat Rana <ranasaugat9@gmail.com>
2025-06-08 15:23:47 -0400

    Finished creating Localstorage for our functionalities

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
 import './App.css'
 
@@ -7,22 +7,31 @@ function App() {
   const addTodo=(todo)=>{
       setTodos((prev)=>[{id:Data.now(),...todo},...prev])
   }
-
   const updateTodo=(todo)=>{
 
     setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id===id? todo : prevTodo)))
   }
-
   const deleteTodo=(id)=>{
     setTodos((prev)=>prev.filter((todo)=>todo.id!==id))
   }
-
   const toggleComplete =(id)=>{
-
     setTodos((prev)=>prev.map((prevTodo)=>prevTodo==id?{...prevTodo, completed:!prevTodo.completed}: prevTodo))
-
   }
 
+    useEffect(()=>{
+     const todos= JSON.parse(localStorage.getItem("todos"))
+
+     if(todos && todos.length > 0){
+
+      setTodos(todos)
+
+
+     }
+    },[])
+
+    useEffect(()=>{
+      localStorage.setItem("todos",JSON.stringify(todos))
+    },[todos])
 
 
 
