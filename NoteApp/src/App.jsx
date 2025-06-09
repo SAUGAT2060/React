96bc713a643395562e803b7ac29edd51eae8fe05
Saugat Rana <ranasaugat9@gmail.com>
2025-06-09 12:58:54 -0400

    Worked on Manually hardcoded Notes and displayed them

--- NoteApp/src/App.jsx
+++ NoteApp/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import  React ,{useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [note, setNotes] = useState([{
+          id:1,
+          title:"Note One",
+           content: "This is my first note"
+
+  },{
+           id:2,
+          title:"Note two",
+           content: "This is my second note"
+  },
+{
+          id:3,
+          title:"Note three",
+           content: "This is my third note"
+}])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+     {(note.map((notes)=>(
+      <React.Fragment key={notes.id}>
+       <div >Id:{notes.id}</div>
+      <h1>{notes.title}</h1>
+     
+      <h2>{notes.content}</h2>
+      </React.Fragment>
+
+     )))}
+    </div>
   )
 }
 
