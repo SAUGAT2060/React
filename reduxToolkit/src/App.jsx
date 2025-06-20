e0c4e7fadfbaabcdcb265822c83115f527168ffb
Saugat Rana <ranasaugat9@gmail.com>
2025-06-20 13:09:58 -0400

    React Reduxtoolkit

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -6,7 +6,7 @@ import AddTodo from './components/AddTodo'
 import Todo from './components/Todo'
 function App() {
   
-  
+
   return (
     <>
      <h1>learn about redux tool kit </h1>
--- reduxToolkit/src/components/AddTodo.jsx
+++ reduxToolkit/src/components/AddTodo.jsx
@@ -3,7 +3,7 @@ import {useDispatch} from 'react-redux'
 import {addTodo} from '../features/Todo/todoSlice'
 function AddTodo() {
  
- const addTodoHandler = ()=>{
+
 
     const [input,setInput] = useState('')
     const dispatch = useDispatch()
@@ -15,21 +15,22 @@ function AddTodo() {
     }
  
   return (
-    <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
-      <input 
-      type="text" 
-      className='bg-gray-800 rounded border border-gray-700 focus::border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out' 
-      placeholder='Enter the value'
-      value={input}
-      onChange={(e)=>setInput(e.target.value)}
-       />
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button
-      type='submit'>
-        Click me 
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Add Todo
       </button>
-
     </form>
   )
-}
+
 }
 export default AddTodo
