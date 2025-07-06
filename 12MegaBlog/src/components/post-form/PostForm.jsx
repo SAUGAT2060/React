55e15e2acf4b0169312035a16ec84a38e2492efc
Saugat Rana <ranasaugat9@gmail.com>
2025-07-06 10:54:51 -0400

    Post Form

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -20,6 +20,75 @@ function PostForm({post}) {
 
   const navigate = useNavigate()
   const userData = useSelector( state => state.user.userData)
+
+  const submit = async (data) =>{
+    if(post){
+    const file = data.image[0] ? appwriteService.uploadFile(data.image[0]):null
+
+    if(file){
+      appwriteService.deleteFile(post.featuredImage)
+    }
+    const dbPost = await appwriteService.updatePost(post.$id,{
+      ...data,
+      featuredImage: file ? file.$id : undefined,
+ })
+      if(dbPost){
+        navigate(`/post/${dbPost.$id}`)
+      }
+
+    
+   
+  }
+  else{
+
+    const file = data.image[0]? await appwriteService.uploadPost(data.image[0]):null;
+
+    if(file){
+     const fileId =file.$id
+     data.featuredImage = file.$id
+    const dbPost = await appwriteService.createPost({
+      ...data,
+      userId:userData.$id,
+     })
+
+     if(dbPost){
+      navigate(`/post/${dbPost.$id}`)
+     }
+    }
+
+  }
+
+  const slugTransform = useCallback((value)=>{
+    if(value && typeof value === 'string ')
+      return value
+      .trim()
+      .toLowerCase()
+      .replace(/^[a-zA-Z\d\s]+/g, '-')
+      .replace(/\s/g,'-')
+      
+    
+    return ''
+  },[])
+
+
+  React.useEffect(()=>{
+    
+    const subscription = watch((value,{name})=>{
+
+      if(name === 'title'){
+        setValue('slug',slugTransform(value.title,{shouldVlaidate:true}))
+      }
+    })
+
+    return ()=>{
+      subscription.unsubscribe()
+    }
+
+
+  },[watch,slugTransform,setValue])
+
+
+  }
   return (
     <div>PostForm</div>
   )
